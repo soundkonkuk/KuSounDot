203bca553a5b93f725ef0894efc8cc10ff568b9c
jeawoo <jjwo97@naver.com>
2021-05-05 00:59:43 +0900

    pull update

--- SoundApp2/app/src/main/java/kr/co/jolph/soundapp/home.kt
+++ SoundApp2/app/src/main/java/kr/co/jolph/soundapp/home.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +36,7 @@ class home : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
     }
 
     companion object {
